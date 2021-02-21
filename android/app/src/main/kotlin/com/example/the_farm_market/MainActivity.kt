28ff02a082e251b5030ff9dd719eecc309a4e7ab
Mano Vikram <manovik18@gmail.com>
2021-02-21 13:01:37 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/the_farm_market/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_farm_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "The Farm Market",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      darkTheme: ThemeData.dark(),
+      home: TheFarm(),
+      routes: {},
+    );
+  }
+}
+
+class TheFarm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+void main() => runApp(MyApp());
