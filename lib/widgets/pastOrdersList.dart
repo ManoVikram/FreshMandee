5272f695188d09c1e3b64d0b477ce77f81475fdc
Mano Vikram <manovik18@gmail.com>
2021-05-05 21:12:54 +0530

    Some minor changes

--- lib/widgets/pastOrdersList.dart
+++ lib/widgets/pastOrdersList.dart
@@ -16,10 +16,24 @@ class PastOrdersList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productDetailsBloc = BlocProvider.of<ProductDetailsBloc>(context);
+    final completeUserDataBloc = BlocProvider.of<CompleteUserDataBloc>(context);
+
+    Future<void> fetchProductDetails(dynamic product) async {
+      await productDetailsBloc.add(
+        FetchProductDetails(
+          firebaseUID: FirebaseAuth.instance.currentUser.uid,
+          productID: product.productID,
+        ),
+      );
+
+      await Future.delayed(
+        Duration(seconds: 3),
+      );
+    }
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: BlocBuilder<CompleteUserDataBloc, CompleteUserDataState>(
+      /* child: BlocBuilder<CompleteUserDataBloc, CompleteUserDataState>(
         builder: (context, completeUserDataState) {
           return completeUserDataState?.userData == null
               ? Center(
@@ -76,7 +90,95 @@ class PastOrdersList extends StatelessWidget {
                           ?.userData?.productsBought?.length,
                     ));
         },
-      ),
+      ), */
+      /* child: completeUserDataBloc?.state == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : (completeUserDataBloc?.state?.userData?.productsBought?.length == 0
+              ? Center(
+                  child: Text(
+                    "No orders yet. Place some new orders.",
+                    style: TextStyle(
+                      fontFamily: GoogleFonts.oxygenMono().fontFamily,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                )
+              : ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (contxt, index) {
+                    productDetailsBloc.add(
+                      FetchProductDetails(
+                        firebaseUID: FirebaseAuth.instance.currentUser.uid,
+                        productID: completeUserDataBloc
+                            ?.state?.userData?.productsBought[index]?.productID,
+                      ),
+                    );
+
+                    print("Hello $index");
+
+                    print(completeUserDataBloc
+                        ?.state?.userData?.productsBought[index]?.productID);
+                    print(productDetailsBloc?.state?.productData?.productID);
+
+                    return productDetailsBloc?.state == null
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : (productDetailsBloc?.state?.productData == null
+                            ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : PastOrderListTile(
+                                image:
+                                    // "https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png",
+                                    productDetailsBloc
+                                        ?.state?.productData?.image,
+                                title: productDetailsBloc
+                                    ?.state?.productData?.name,
+                                quantity: completeUserDataBloc?.state?.userData
+                                    ?.productsBought[index]?.quantity,
+                                price: productDetailsBloc
+                                    ?.state?.productData?.price,
+                              ));
+                  },
+                  itemCount: completeUserDataBloc
+                      ?.state?.userData?.productsBought?.length,
+                )), */
+      child: completeUserDataBloc?.state?.userData == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Builder(
+              builder: (contxt) {
+                List<PastOrderListTile> productsList = [];
+
+                for (var product
+                    in completeUserDataBloc?.state?.userData?.productsBought) {
+                  fetchProductDetails(product);
+
+                  productsList.add(
+                    PastOrderListTile(
+                      image:
+                          // "https://i.pinimg.com/originals/cb/33/49/cb3349b86ca661ca61ae9a36d88d70d4.png",
+                          productDetailsBloc?.state?.productData?.image,
+                      title: productDetailsBloc?.state?.productData?.name,
+                      quantity: product?.quantity,
+                      price: productDetailsBloc?.state?.productData?.price,
+                    ),
+                  );
+                }
+
+                return ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => productsList[index],
+                  itemCount: productsList.length,
+                );
+              },
+            ),
     );
   }
 }
--- mano_vikram_portfolio/android/app/src/main/kotlin/com/example/mano_vikram_portfolio/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mano_vikram_portfolio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
